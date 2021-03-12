f0ad5b2d7611b9e462944c30c78b0df1e5ce0964
Muhammad Babayi <chief.mb1976@gmail.com>
2021-03-12 11:56:07 +0330

    move PagesViewers to PagesViewers namespace

--- MangaReader/Views/Components/PagesViewers/PageSingle.xaml.cs
+++ MangaReader/Views/Components/PagesViewers/PageSingle.xaml.cs
@@ -7,7 +7,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 
-namespace MangaReader.Views.Components
+namespace MangaReader.Views.Components.PagesViewers
 {
     /// <summary>
     /// Interaction logic for PageSingle.xaml
